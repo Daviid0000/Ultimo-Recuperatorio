7d1c631b0ee2512fd475694f0f8d7b1e8bb3a930
David Liva <davidliva01@gmail.com>
2023-08-02 16:14:39 -0300

    Quinto commit, se desarrollaron los controladores para el CRUD de reservas.

--- controllers/reserva.controllers.js
+++ controllers/reserva.controllers.js
@@ -1,4 +1,5 @@
 // TODO: Crear controladores para cada una de las rutas de reserva.
+const Reserva = require('../models/Reserva');
 const ctrl = {};
 
 // ==========================================
@@ -6,9 +7,7 @@ const ctrl = {};
 // ==========================================
 
 // Obtener todas las reservas
-ctrl.renderListaReservas = (req, res) => {
-    res.render('listado-reserva')
-}
+ctrl.renderListaReservas 
 // Formulario para crear una reserva
 ctrl.renderNuevaReserva
 
@@ -20,19 +19,111 @@ ctrl.renderEditarReserva,
 // ==========================================
 
 // Obtener todas las reservas
-ctrl.obtenerReservas
+ctrl.obtenerReservas = async (req, res) => {
+    try {
+        const reservas = await Reserva.findAll({
+            where: {
+                estado: true
+            }
+        });
+
+        return res.json(reservas);
+    } catch (error) {
+        console.log('Error al obtener las reservas', error);
+        return res.status(500).json({
+            message: 'Error al obtener las reservas'
+        })
+    }
+}
 
 // Obtener una reserva
-ctrl.obtenerUnaReservas
+ctrl.obtenerUnaReserva = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const reserva = await Reserva.findOne({
+            whare: {
+                estado: true,
+                id
+            }
+        });
+        return res.json(reserva);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Error al obtener la reserva'
+        })
+    }
+}
 
 // Crear una reserva
-ctrl.crearUnaReserva
+ctrl.crearUnaReserva = async (req, res) => {
+    const {
+        nombre,
+        apellido,
+        fechaTiket,
+        pelicula,
+        precio
+    } = req.body;
+
+    try {
+        // Se crea una nueva instancia de reserva
+        const nuevaReserva = new Reserva({
+            nombre,
+            codigo: new Date().getTime(),
+            apellido,
+            fechaTiket,
+            pelicula,
+            precio
+        });
+
+        // Se guarda en la BD
+        await nuevaReserva.save();
+
+        return res.status(201).json({ message: 'Reserva creada con éxito' })
+    } catch (error) {
+        console.log('Error al crear la reserva', error);
+        return res.status(500).json({ message: 'Error al crear la reserva' })
+    }
+}
 
 // Actualizar una reserva
-ctrl.actualizarReserva
+ctrl.actualizarReserva = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const reserva = await Reserva.findByPk(id);
+        await reserva.update(req.body)
+        return res.json({
+            message: 'Reserva actualizada exitosamente'
+        });
+    } catch (error) {
+        console.log('Error al actualizar la reserva', error);
+        return res.status(error.status || 500).json({
+            message: error.message
+        })
+    }
+}
 
 // Eliminar una reserva de forma lógica
-ctrl.eliminarUnaReserva
+ctrl.eliminarUnaReserva = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if(!id){
+            throw({
+                status: 400,
+                message: 'No se ha enviado el id de la reserva'
+            })
+        }
+        const reserva = await Reserva.findByPk(id);
+        await reserva.update({ estado: false });
+        return res.json({ message: 'Reserva se eliminó correctamente' })
+    } catch (error) {
+        console.log('Error al eliminar la reserva', error);
+        return res.status(error.status || 500).json({
+            message: error.message || 'Error al eliminar la reserva'
+        })
+    }
+}
+
 
 
 module.exports = ctrl;
